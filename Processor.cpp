18ef1a0599644afb6cc40f83a7918211b7e0bc3e
aliatarek <115508762+aliatarek@users.noreply.github.com>
2023-04-12 16:23:52 +0200

    Create Processor.cpp

---
+++ Processor.cpp
@@ -0,0 +1,42 @@
+#include "Processor.h"
+
+//Process* Processor::getRun()
+//{
+//	return nullptr;
+//}
+
+Processor::Processor()
+{
+	Run = nullptr;
+	ID = 0;
+	SchedulingState = 0;
+	ProcessorBusyTime = 0;
+	PLoad = 0;
+	PUtil = 0;
+	FinishTime = 0;
+	//setting queue? its already done in class queue?
+}
+void Processor::EnqueueReady(Process p)
+{
+	//by reference?
+	RDY.enqueue(p); //function in queue takes it by reference?
+}
+
+void Processor::setSchedulingState(bool u)
+{
+	u = SchedulingState;
+}
+
+bool Processor::getSchedulingState()
+{
+	return false;
+}
+
+void Processor::EnqueueReady(Process p)
+{
+}
+
+int Processor::getFinishTime()
+{
+	return 0;
+}
